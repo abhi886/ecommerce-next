4e4ecf14aa10bd5e1a4e62a23750dcc72c49ec5b
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-05-07 12:06:36 +1000

    Footer

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { FC, ReactNode } from "react";
 import s from "./Layout.module.css";
+import { Footer } from "@components/common";
 
 function Layout({ children }) {
   return (
     <div className={s.root}>
       <main className='fit'>{children}</main>
+      <Footer></Footer>
     </div>
   );
 }
