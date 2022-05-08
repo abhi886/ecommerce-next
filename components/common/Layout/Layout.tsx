bd928f5ba0e00169300c2a7b72d4b69873a0c73e
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-05-08 11:43:14 +1000

    Wishlist and sidebar

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,18 @@
 import { FC, ReactNode } from "react";
 import s from "./Layout.module.css";
 import { Footer, Navbar } from "@components/common";
+import { CartSidebar } from "@components/cart"
+import { Sidebar } from "@components/ui"
+
+
 
 function Layout({ children }) {
   return (
     <div className={s.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className='fit'>{children}</main>
       <Footer></Footer>
     </div>
