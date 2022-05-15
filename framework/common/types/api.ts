74d4952d8b70046afc3174c179f2f719b7bd0349
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-05-15 12:14:46 +1000

    Specify Variable Type

--- framework/common/types/api.ts
+++ framework/common/types/api.ts
@@ -1,9 +1,11 @@
 export type ApiFetcherOptions = {
   url: string;
   query: string;
-  variables?: any;
+  variables?: Variables; // variable is a key value pair
 };
-
+export type Variables = {
+  [key: string]: string;
+};
 export type ApiFetcherResults<T> = {
   data: T;
 };
