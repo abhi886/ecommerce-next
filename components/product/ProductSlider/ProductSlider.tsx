ffb3fc64f2de8cf5a90e1ffd9bbe49dcac22cf23
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-05-22 12:03:03 +1000

    146. Keen Slider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,10 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, Children, isValidElement } from "react";
 import s from "./ProductSlider.module.css";
 
 const ProductSlider: FC = ({ children }) => {
   return (
     <div className={s.root}>
-      <div className='h-full transition-opacity duration-150'>{children}</div>
+      <div className='keen-slider h-full transition-opacity duration-150'>
+        {Children.map(children, (child) => {
+          if (isValidElement(child)) {
+            console.log(child);
+            return {
+              ...child,
+              props: {
+                ...child.props,
+                className: "keen-slider__slide",
+              },
+            };
+            // return React.cloneElement(child, {
+            //   className: "keen-slider__slide",
+            // });
+          }
+          return child;
+        })}
+      </div>
     </div>
   );
 };
