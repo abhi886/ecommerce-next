d5d12f6f35fee8d59d2608b3b56d49c3112c17d3
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-05-22 12:41:40 +1000

    Sliding in keen slider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,23 +1,28 @@
 import React, { FC, Children, isValidElement } from "react";
 import s from "./ProductSlider.module.css";
+import { useKeenSlider } from "keen-slider/react";
 
 const ProductSlider: FC = ({ children }) => {
+  const [sliderRef, _] = useKeenSlider({
+    initial: 0,
+    slideChanged(s) {
+      console.log("changing to slide:");
+    },
+    loop: true,
+  });
+
   return (
-    <div className={s.root}>
+    <div ref={sliderRef as any} className={s.root}>
       <div className='keen-slider h-full transition-opacity duration-150'>
         {Children.map(children, (child) => {
           if (isValidElement(child)) {
-            console.log(child);
-            return {
-              ...child,
-              props: {
-                ...child.props,
-                className: "keen-slider__slide",
-              },
-            };
-            // return React.cloneElement(child, {
-            //   className: "keen-slider__slide",
-            // });
+            return React.cloneElement(child, {
+              className: `${
+                child.props.className
+                  ? `${child.props.className} keen-slider__slide`
+                  : ""
+              }`,
+            });
           }
           return child;
         })}
