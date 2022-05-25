2a6e9f9b03e46c3ca9d84e1cfbf5a510e169b7b5
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-05-25 11:45:36 +1000

    Product slider done

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,19 +1,23 @@
 import React, { FC, Children, isValidElement } from "react";
 import s from "./ProductSlider.module.css";
 import { useKeenSlider } from "keen-slider/react";
+import cn from "classnames";
 
 const ProductSlider: FC = ({ children }) => {
-  const [sliderRef, _] = useKeenSlider({
+  const [sliderRef, slider] = useKeenSlider({
     initial: 0,
-    slideChanged(s) {
-      console.log("changing to slide:");
-    },
+    slideChanged() {},
     loop: true,
   });
 
   return (
     <div ref={sliderRef as any} className={s.root}>
-      <div className='keen-slider h-full transition-opacity duration-150'>
+      <div className='keen-slider h-full transition-opacity'>
+        <button
+          className={cn(s.leftControl, s.control)}
+          onClick={(e) => slider.current?.prev()}
+        />
+
         {Children.map(children, (child) => {
           if (isValidElement(child)) {
             return React.cloneElement(child, {
@@ -26,6 +30,10 @@ const ProductSlider: FC = ({ children }) => {
           }
           return child;
         })}
+        <button
+          className={cn(s.rightControl, s.control)}
+          onClick={(e) => slider.current?.next()}
+        />
       </div>
     </div>
   );
