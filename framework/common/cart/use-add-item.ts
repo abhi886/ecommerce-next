85f5874123ee230a4a8fcd7acaad7a72ce00c979
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-08-21 12:04:01 +1000

    Lecture 168. Use Hook

--- framework/common/cart/use-add-item.ts
+++ framework/common/cart/use-add-item.ts
@@ -1,6 +1,7 @@
 import { handler } from "@framework/cart/use-add-item";
-
+import { useHook } from "@common/utils/use-hook";
 const useAddItem = () => {
-  return handler.useHook();
+  const hook = useHook((hooks) => hooks.cart.useAddItem);
+  return hook.useHook();
 };
 export default useAddItem;
