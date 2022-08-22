f1e3a3c3f760461f7d998117f264183e2d19becf
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-08-22 11:28:37 +1000

    170. Mutation Hooks

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,7 +1,8 @@
 import { useAddItem } from "@common/cart";
+import { MutationHook } from "@common/types/hooks";
 export default useAddItem;
 
-export const handler = {
+export const handler: MutationHook = {
   fetcher: (input: any) => {
     return JSON.stringify(input) + "_Modified";
   },
